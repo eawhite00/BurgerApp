f72215bba459db85e76c4c5a0ca90ad44ae258bc
eawhite00 <eawhite00@gmail.com>
2020-02-28 14:03:37 -0800

    set up model and controller

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('katsu burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('normal burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('dark burger', false);
