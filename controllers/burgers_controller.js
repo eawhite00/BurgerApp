c74c015482ff39a1f6b65c34cefba43f27c6cced
eawhite00 <eawhite00@gmail.com>
2020-02-28 16:49:36 -0800

    all functionality added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,40 +3,43 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name"
-  ], [
-    req.body.name
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
 
-  console.log("condition", condition);
+    console.log(req.body);
+    var id = "id = " + req.params.id;
+    var condition = req.body;
 
-  burger.update(condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+    console.log("condition", condition);
+
+    burger.update(condition, id, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
 module.exports = router;
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $(".devour").on("click", function(event) {
+        var id = $(this).data("id");
+    
+        var newDevouredState = {
+          devoured: true
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+        }).then(
+          function() {
+            console.log("changed devoured to: ", true);
+            // Reload the page to get the updated list
+            location.reload(); 
+          }
+        );
+      });
+
+      $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        var newBurger = {
+          burger_name: $("#burgerInput").val().trim(),
+          devoured: false
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+
+});
